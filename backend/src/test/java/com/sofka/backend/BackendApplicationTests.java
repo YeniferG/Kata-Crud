3b2bacb4e1fb13bca8fa04a45127fc036b1b1f4a
Yenifer <guajeyenifer@gmail.com>
2021-11-26 22:24:45 -0500

    Creación inicial del proyecto

---
+++ backend/src/test/java/com/sofka/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
