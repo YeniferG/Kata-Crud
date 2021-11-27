510f09369b00864489a39a1deb268a84e3d518cf
Yenifer <guajeyenifer@gmail.com>
2021-11-27 11:01:58 -0500

    Se realiza la creación de un formulario que se va a encargar de realizar las creaciones del proyecto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer, useEffect } from 'react';
+import React, { useContext, useReducer, useEffect, useRef, useState } from 'react';
 
 const HOST_API = "http://localhost:8080/api"
 
@@ -7,6 +7,43 @@ const initialState = {
 };
 
 const Store = createContext(initialState)
+
+const Form = () => {
+  const formRef = useRef(null);
+  const { dispatch } = useContext(Store);
+  const { state, setState} = useState({});
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted: false
+    };
+
+    fetch(HOST_API+"/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((todo) => {
+      dispatch({type: "add-item", item: todo});
+      setState({name: ""});
+      formRef.current.reset();
+    });
+  }
+
+  return <form ref={formRef}>
+    <input type="text" name="name" onChange={(event) => {
+      setState({ ...state, name: event.target.value })
+    }}></input>
+    <button onClick={onAdd}>Agregar</button>
+  </form>
+}
 
 const List = () => {
   const { dispatch, state } = useContext(Store);
